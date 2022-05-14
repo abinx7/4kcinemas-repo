37f69be5953bdc980e7c5a3ac918519a6141fd0f
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-05-14 07:25:10 +0530

    Update shortner.py

--- plugins/modules/shortner.py
+++ plugins/modules/shortner.py
@@ -15,7 +15,7 @@ OWLY_API = os.environ.get("OWLY_API", None)
 BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text='⚙ ᴅᴋ ʙᴏᴛx ⚙', url='https://t.me/dk_botx')
+            InlineKeyboardButton(text='SS_Linkz', url='https://t.me/SS_Linkz')
         ]
     ]
 )
